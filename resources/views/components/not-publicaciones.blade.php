2381b05808f7d03f1702fc5acbeb760e980a9723
Tomasito <liberfran.04@gmail.com>
2025-03-06 12:07:50 -0300

    Ajustes y validaciones extras para mejorar el codigo

--- resources/views/components/not-publicaciones.blade.php
+++ resources/views/components/not-publicaciones.blade.php
@@ -1,14 +1,7 @@
-<div class="h-uto">
-    <div class="mt-20 mx-5">
-        <h1 class="text-4xl text-center text-white/75">Aun no hay publicaciones de este usuario</h1>
-    </div>
-    @auth
-        @if (Auth::user()->id === $userId)
-            <div class="flex flex-col justify-stretch h-screen items-center space-y-5 h-full">
-                <div class="space-y-5 flex flex-col items-center">
-                    <x-publi-button />
-                </div>
-            </div>
-        @endif
-    @endauth
+<div class="h-auto">
+    @if (!request()->is('home'))
+        <div class="mt-20 mx-5">
+            <h1 class="text-4xl text-center text-white/75">Aun no hay publicaciones de este usuario</h1>
+        </div>
+    @endif
 </div>
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,25 +1,42 @@
+@php
+    # Verifico si existen usuarios relacionados con posts
+    $posts_avail = false;
+
+    for ($i = 0; $i < $posts->count(); $i++) {
+        if ($posts[$i]->user) {
+            // dd('nada');
+            $posts_avail = true;
+        }
+    }
+@endphp
+
 <x-layout>
     <section class="flex mt-10 flex-col mx-3">
         <div class="mx-3 mt-10">
             <h1 class="text-4xl">Página principal</h1>
         </div>
-        @if (!$posts)
-            <x-not-publicaciones>
-                <x-slot:userId>{{ $user->id }}</x-slot:userId>
-            </x-not-publicaciones>
+        @if (!$posts_avail)
+            <x-not-publicaciones />
         @else
-            <!--filtro los usuarios con posts-->
+            <!--filtro ÚNICAMENTE los usuarios con posts
+                los posts sin usuario
+                habia un error que cuando eliminabas un usuario, su post seguí estando
+                cuando realmente deberian eliminarse ambas cosas (en la base de datos se refleja bien, pero en el entorno de desarrollo no)
+            -->
             @foreach ($posts as $post)
-                <x-publicaciones>
-                    <x-slot:profilePhoto>{{ $post->user->profile_photo }}</x-slot:profilePhoto>
-                    <x-slot:username>{{ $post->user->name }}</x-slot:username>
-                    <x-slot:userId>{{ $post->user->id }}</x-slot:userId>
-                    <x-slot:postId>{{ $post->id }}</x-slot:postId>
-                    <x-slot:postImage>{{ $post->image }}</x-slot:postImage>
-                    <x-slot:postDescripcion>{{ $post->descripcion }}</x-slot:postDescripcion>
-                    <x-slot:postCreado>{{ $post->created_at }}</x-slot:postCreado>
-                    <x-slot:postActualizado>{{ $post->updated_at }}</x-slot:postActualizado>
-                </x-publicaciones>
+                {{-- @dd($post->user->profile_photo) --}}
+                @if ($post->user)
+                    <x-publicaciones>
+                        <x-slot:profilePhoto>{{ $post->user->profile_photo }}</x-slot:profilePhoto>
+                        <x-slot:username>{{ $post->user->name }}</x-slot:username>
+                        <x-slot:userId>{{ $post->user->id }}</x-slot:userId>
+                        <x-slot:postId>{{ $post->id }}</x-slot:postId>
+                        <x-slot:postImage>{{ $post->image }}</x-slot:postImage>
+                        <x-slot:postDescripcion>{{ $post->descripcion }}</x-slot:postDescripcion>
+                        <x-slot:postCreado>{{ $post->created_at }}</x-slot:postCreado>
+                        <x-slot:postActualizado>{{ $post->updated_at }}</x-slot:postActualizado>
+                    </x-publicaciones>
+                @endif
             @endforeach
             <x-modal>
                 <x-slot:modalTitle>Eliminar publicación</x-slot:modalTitle>
